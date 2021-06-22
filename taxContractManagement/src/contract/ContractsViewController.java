87597640c6cfc46986fb66c3b2446e22acfddb61
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-06-22 01:20:26 +0100

    last push

--- taxContractManagement/src/contract/ContractsViewController.java
+++ taxContractManagement/src/contract/ContractsViewController.java
@@ -7,7 +7,11 @@ package contract;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,6 +20,15 @@ import javafx.fxml.Initializable;
  */
 public class ContractsViewController implements Initializable {
 
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane editPane;
+    @FXML
+    private Pane editPane1;
+    @FXML
+    private AnchorPane contractsAnchor;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +36,17 @@ public class ContractsViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void getContractsTable(MouseEvent event) {
+    }
+
+    @FXML
+    private void statView(MouseEvent event) {
+    }
+
+    @FXML
+    private void getAddContractsView(MouseEvent event) {
+    }
     
 }
