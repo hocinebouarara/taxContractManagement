4632d2a06159a2ba7021a343001edc56237256f6
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-09 06:14:57 +0100

    create method for get view get view items

--- taxContractManagement/src/payments/AddFicheControleController.java
+++ taxContractManagement/src/payments/AddFicheControleController.java
@@ -9,6 +9,8 @@ import com.jfoenix.controls.JFXDatePicker;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import helpres.DbConnect;
+import helpres.Links;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,8 +22,11 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
@@ -32,6 +37,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Callback;
 import models.Contract;
 import models.Property;
@@ -376,6 +383,19 @@ public class AddFicheControleController implements Initializable {
 
     @FXML
     private void addContactView(MouseEvent event) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(Links.ADDCONTRACTEVIEW));
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(ProprietorsViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Parent parent = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(parent));
+        stage.initStyle(StageStyle.UTILITY);
+        stage.show();
     }
 
 }
