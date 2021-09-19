9ec2fcc7e6eb90bdb361e87d8903026460f4fb81
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-19 03:37:10 +0100

    data

---
+++ taxContractManagement/src/Outils/Data.java
@@ -0,0 +1,22 @@
+package Outils;
+
+public class Data {
+    int imgsize;
+    int fieldpro;
+
+    public double getImgsize() {
+        return imgsize;
+    }
+
+    public void setImgsize(int imgsize) {
+        this.imgsize = imgsize;
+    }
+
+    public double getFieldpro() {
+        return fieldpro;
+    }
+
+    public void setFieldpro(int fieldpro) {
+        this.fieldpro = fieldpro;
+    }
+}
