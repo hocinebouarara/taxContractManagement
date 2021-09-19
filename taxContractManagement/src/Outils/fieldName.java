607196228525ef82853ccb4e760b948fc3ebad7f
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-19 03:32:12 +0100

    fields names

---
+++ taxContractManagement/src/Outils/fieldName.java
@@ -0,0 +1,15 @@
+package Outils;
+
+public enum fieldName {
+    Derection_impots,
+    Wilaya,
+    Inspection,
+    Recette_des_impots,
+    Nom_et_prenom_bailleur,
+    Adress_bailleur,
+    N_id_fiscal,
+    N_articlage,
+    Adress_bien_loue,
+    Montant_monsuel,
+    Nom_prenom_preneur,
+    }
