eb46b261ac86bdb786166fb3ab3e779f11ae4184
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-19 03:36:40 +0100

    convert data to CSV

---
+++ taxContractManagement/src/Outils/CreateDataset.java
@@ -0,0 +1,55 @@
+package Outils;
+
+import java.io.*;
+
+public class CreateDataset {
+    
+    public static void createCSV(String name){
+        StringBuilder builder=new StringBuilder();
+        builder.append("imageswidth").append(",").append("imagesheight").append(",").append("Name").append(",").append("XOffset").append(",").append("YOffset").append(",").append("width").append(",").append("height");
+        try (FileWriter fileWriter = new FileWriter("C:\\Users\\Deeplight\\OneDrive\\Documents\\NetBeansProjects\\taxContractManagement\\taxContractManagement\\taxContractManagement\\src\\Dataset\\G51\\Dataset\\"+name)){
+
+            fileWriter.write(builder.toString());
+
+        }catch (Exception e){
+
+        }
+    }
+    public static void createCSVforfield(String imagesize,String feildpropertie ,String url){
+        StringBuilder builder=new StringBuilder();
+        builder.append(imagesize).append(",").append(feildpropertie);
+        try (FileWriter fileWriter = new FileWriter(url)){
+
+            fileWriter.write(builder.toString());
+
+        }catch (Exception e){
+
+        }
+    }
+    public static void addtoCsv(double imgw, double imgh, String Name, float x, float y, float w, float h){
+
+        try {
+            FileWriter fileWriter=new FileWriter("C:\\Users\\Deeplight\\OneDrive\\Documents\\NetBeansProjects\\taxContractManagement\\taxContractManagement\\taxContractManagement\\src\\Dataset\\G51\\Dataset\\Dataset.csv",true);
+            BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+            PrintWriter printWriter=new PrintWriter(bufferedWriter);
+            printWriter.println(imgw+","+imgh+","+Name+","+x+","+y+","+w+","+h);
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void addtoCsv(int imgsize,int imglocate,String url){
+
+        try {
+            FileWriter fileWriter=new FileWriter(url,true);
+            BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+            PrintWriter printWriter=new PrintWriter(bufferedWriter);
+            printWriter.println(imgsize+","+imglocate);
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
