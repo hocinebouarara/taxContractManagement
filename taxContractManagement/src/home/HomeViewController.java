64442a3f52d26458ae8b4a57079c46cde3b9095d
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-19 03:25:44 +0100

    update design of home add button for get scan documents

--- taxContractManagement/src/home/HomeViewController.java
+++ taxContractManagement/src/home/HomeViewController.java
@@ -41,6 +41,8 @@ public class HomeViewController implements Initializable {
     private Button paymentBtn;
     @FXML
     private AnchorPane anchor;
+    @FXML
+    private Button documentsBtn;
 
     /**
      * Initializes the controller class.
@@ -61,6 +63,7 @@ public class HomeViewController implements Initializable {
         benefiBtn.setStyle("-fx-background-color:transparent;");
         contratBtn.setStyle("-fx-background-color:transparent;");
         paymentBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         homeBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 16;-fx-border-radius:18px;");
     }
@@ -72,6 +75,7 @@ public class HomeViewController implements Initializable {
         benefiBtn.setStyle("-fx-background-color:transparent;");
         contratBtn.setStyle("-fx-background-color:transparent;");
         paymentBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         propaBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 24;-fx-border-radius:18px;");
 
@@ -85,9 +89,10 @@ public class HomeViewController implements Initializable {
         contratBtn.setStyle("-fx-background-color:transparent;");
         benefiBtn.setStyle("-fx-background-color:transparent;");
         paymentBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         proprBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 18;-fx-border-radius:18px;");
-        
+
         loadViews(Links.PROPIETORVIEW);
     }
 
@@ -98,6 +103,7 @@ public class HomeViewController implements Initializable {
         proprBtn.setStyle("-fx-background-color:transparent;");
         benefiBtn.setStyle("-fx-background-color:transparent;");
         paymentBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         contratBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 25;-fx-border-radius:18px;");
 
@@ -111,9 +117,10 @@ public class HomeViewController implements Initializable {
         proprBtn.setStyle("-fx-background-color:transparent;");
         contratBtn.setStyle("-fx-background-color:transparent;");
         paymentBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         benefiBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 18;-fx-border-radius:18px;");
-        
+
         loadViews(Links.BENEFICIAIREVIEW);
 
     }
@@ -125,16 +132,33 @@ public class HomeViewController implements Initializable {
         proprBtn.setStyle("-fx-background-color:transparent;");
         contratBtn.setStyle("-fx-background-color:transparent;");
         benefiBtn.setStyle("-fx-background-color:transparent;");
+        documentsBtn.setStyle("-fx-background-color:transparent;");
 
         paymentBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 10;-fx-border-radius:18px;");
-       
+
         loadViews(Links.PAYMENTSVIEW);
+    }
+
+    @FXML
+    private void getDocumentsView(MouseEvent event) {
+        propaBtn.setStyle("-fx-background-color:transparent;");
+        homeBtn.setStyle("-fx-background-color:transparent;");
+        proprBtn.setStyle("-fx-background-color:transparent;");
+        contratBtn.setStyle("-fx-background-color:transparent;");
+        benefiBtn.setStyle("-fx-background-color:transparent;");
+        paymentBtn.setStyle("-fx-background-color:transparent;");
+
+        documentsBtn.setStyle("-fx-text-fill:white;-fx-background-color:#FFA000;-fx-border-color:#FFA000;-fx-border-width:0 10 0 10;-fx-padding:0 0 0 10;-fx-border-radius:18px;");
+
+        loadViews(Links.DEMATERIALIZATIONVIEW);
+
     }
 
     private void loadViews(String viewName) {
         try {
             AnchorPane pane = FXMLLoader.load(getClass().getResource(viewName));
             anchor.getChildren().setAll(pane);
+
         } catch (IOException ex) {
             Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
         }
