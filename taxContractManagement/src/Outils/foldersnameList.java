27f54fa7965b95775d6bda6255fbc30dd741b01d
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-09-19 03:31:55 +0100

    folders names

---
+++ taxContractManagement/src/Outils/foldersnameList.java
@@ -0,0 +1,23 @@
+package Outils;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class foldersnameList {
+
+    public static ObservableList<String> list= FXCollections.observableArrayList(
+                 "Derection_impots",
+                        "Wilaya",
+                        "Inspection",
+                        "Recette_des_impots",
+                        "Nom_et_prenom_bailleur",
+                        "Adress_bailleur",
+                        "N_id_fiscal",
+                        "N_articlage",
+                        "Adress_bien_loue",
+                        "Montant_monsuel",
+                        "Nom_prenom_preneur",
+                        "Occupation_preneur"
+                        );
+
+}
